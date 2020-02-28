ba8c5ccc580a9c86439835fe0ba66f4d0e6ed734
lenovo <lenovo@lenovo-Lenovo-ideapad-320-15IKB>
2020-02-28 16:50:35 +0530

    /Akshay

--- src/main/java/com/ats/hreasy/common/Constants.java
+++ src/main/java/com/ats/hreasy/common/Constants.java
@@ -40,6 +40,23 @@ public class Constants {
 	public static final String imageShowUrl = "http://192.168.1.25:8080/mixDoc/";*/
 	
 	
+	//atsserver
+		/*public static final String url = "http://107.180.88.121:8080/HrEsayWebApi/";
+		public static String REPORT_SAVE = "/opt/apache-tomcat-8.5.47/webapps/hrdocument/Report.pdf"; 
+		public static final String ReportURL = "http://107.180.88.121:8080/HrEasy/";//gfpl 
+		public static final String attsDocSaveUrl = "/opt/apache-tomcat-8.5.47/webapps/hrdocument/attendancedoc/";
+		public static final String docSaveUrl= "/opt/apache-tomcat-8.5.47/webapps/hrdocument/updatedoc/";
+		
+		public static final String companyLogoSaveUrl = "/opt/apache-tomcat-8.5.47/webapps/hrdocument/companylogo/";
+		public static final String empDocSaveUrl = "/opt/apache-tomcat-8.5.47/webapps/hrdocument/empdoc/";
+		public static final String leaveDocSaveUrl = "/opt/apache-tomcat-8.5.47/webapps/hrdocument/leavedoc/"; 
+		public static final String imageSaveUrl = "/opt/apache-tomcat-8.5.47/webapps/hrdocument/mixDoc/";
+		
+		public static final String companyLogoShowUrl = "http://107.180.88.121:8080/companylogo/";
+		public static final String empDocShowUrl = "http://107.180.88.121:8080/empdoc/"; 
+		public static final String leaveDocShowUrl = "http://107.180.88.121:8080/leavedoc/";
+		public static final String imageShowUrl = "http://107.180.88.121:8080/mixDoc/";*/
+	
 	public static RestTemplate rest = new RestTemplate();
 	public static String[] allextension = { "txt", "doc", "pdf", "xls", "jpg", "jpeg", "gif", "png" };
 	public static String[] values = { "jpg", "jpeg", "gif", "png" }; 
