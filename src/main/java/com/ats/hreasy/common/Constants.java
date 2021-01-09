f53b99cd2a8dd65c256dd939e1ffe056d23ad291
ashay <ashay@ashay-Inspiron-5520>
2021-01-09 12:36:46 +0530

    /Akshay

--- src/main/java/com/ats/hreasy/common/Constants.java
+++ src/main/java/com/ats/hreasy/common/Constants.java
@@ -7,10 +7,10 @@ public class Constants {
 
 	// local
 
-	/*public static final String opsWebApiUrl = "http://107.180.95.11:8080/webapi/";
+	public static final String opsWebApiUrl = "http://107.180.95.11:8080/webapi/";
 
 	public static final String url = "http://localhost:8094/";
-	public static String REPORT_SAVE = "/home/lenovo/Documents/pdf/Report.pdf";
+	public static String REPORT_SAVE = "/home/ashay/Documents/PDF/Report.pdf";
 	public static final String ReportURL = "http://localhost:8081/hreasy/";
 	public static final String attsDocSaveUrl = "/home/lenovo/Downloads/old/apache-tomcat-8.5.37/webapps/media/";
 	public static final String docSaveUrl = "/home/lenovo/Downloads/old/apache-tomcat-8.5.37/webapps/media/";
@@ -26,29 +26,39 @@ public class Constants {
 	public static final String leaveDocShowUrl = "http://localhost:8080/media/";
 	public static final String imageShowUrl = "http://localhost:8080/media/";
 	public static final String templateShowUrl = "http://localhost:8080/hrdocument/templatedoc/";
-	public static String EPF_SAVE = "/home/lenovo/Documents/pdf/EPFFILE.txt";*/
+	public static String EPF_SAVE = "/home/ashay/Documents/PDF/EPFFILE.txt";
 
-	//new mumbai
-	public static final String opsWebApiUrl = "http://107.180.95.11:8080/webapi/";
+	// new mumbai
 
-	public static final String url = "http://107.180.72.86:8080/HrEsayWebApi/";
-	public static String REPORT_SAVE = "/opt/cpanel/ea-tomcat85/webapps/hrdocument/Report.pdf";
-	public static final String ReportURL = "http://107.180.72.86:8080/HrEasy/";
-	public static final String attsDocSaveUrl = "/opt/cpanel/ea-tomcat85/webapps/hrdocument/attendancedoc/";
-	public static final String docSaveUrl = "/opt/cpanel/ea-tomcat85/webapps/hrdocument/updatedoc/";
+	/*
+	 * public static final String url = "http://107.180.72.86:8080/HrEsayWebApi/";
+	 * public static String REPORT_SAVE =
+	 * "/opt/cpanel/ea-tomcat85/webapps/hrdocument/Report.pdf"; public static final
+	 * String ReportURL = "http://107.180.72.86:8080/HrEasy/"; public static final
+	 * String attsDocSaveUrl =
+	 * "/opt/cpanel/ea-tomcat85/webapps/hrdocument/attendancedoc/"; public static
+	 * final String docSaveUrl =
+	 * "/opt/cpanel/ea-tomcat85/webapps/hrdocument/updatedoc/";
+	 * 
+	 * public static final String companyLogoSaveUrl =
+	 * "/opt/cpanel/ea-tomcat85/webapps/hrdocument/companylogo/"; public static
+	 * final String empDocSaveUrl =
+	 * "/opt/cpanel/ea-tomcat85/webapps/hrdocument/empdoc/"; public static final
+	 * String leaveDocSaveUrl =
+	 * "/opt/cpanel/ea-tomcat85/webapps/hrdocument/leavedoc/"; public static final
+	 * String imageSaveUrl = "/opt/cpanel/ea-tomcat85/webapps/hrdocument/mixDoc/";
+	 * 
+	 * public static final String companyLogoShowUrl =
+	 * "http://107.180.72.86:8080/hrdocument/companylogo/"; public static final
+	 * String empDocShowUrl = "http://107.180.72.86:8080/hrdocument/empdoc/"; public
+	 * static final String leaveDocShowUrl =
+	 * "http://107.180.72.86:8080/hrdocument/leavedoc/"; public static final String
+	 * imageShowUrl = "http://107.180.72.86:8080/hrdocument/mixDoc/"; public static
+	 * final String templateShowUrl =
+	 * "http://107.180.72.86:8080/hrdocument/templatedoc/"; public static String
+	 * EPF_SAVE = "/opt/cpanel/ea-tomcat85/webapps/hrdocument/EPFFILE.txt";
+	 */
 
-	public static final String companyLogoSaveUrl = "/opt/cpanel/ea-tomcat85/webapps/hrdocument/companylogo/";
-	public static final String empDocSaveUrl = "/opt/cpanel/ea-tomcat85/webapps/hrdocument/empdoc/";
-	public static final String leaveDocSaveUrl = "/opt/cpanel/ea-tomcat85/webapps/hrdocument/leavedoc/";
-	public static final String imageSaveUrl = "/opt/cpanel/ea-tomcat85/webapps/hrdocument/mixDoc/";
-
-	public static final String companyLogoShowUrl = "http://107.180.72.86:8080/hrdocument/companylogo/";
-	public static final String empDocShowUrl = "http://107.180.72.86:8080/hrdocument/empdoc/";
-	public static final String leaveDocShowUrl = "http://107.180.72.86:8080/hrdocument/leavedoc/";
-	public static final String imageShowUrl = "http://107.180.72.86:8080/hrdocument/mixDoc/";
-	public static final String templateShowUrl = "http://107.180.72.86:8080/hrdocument/templatedoc/";
-	public static String EPF_SAVE = "/opt/cpanel/ea-tomcat85/webapps/hrdocument/EPFFILE.txt";
-	
 	// monginispune
 
 	/*
@@ -82,23 +92,34 @@ public class Constants {
 
 	// atsserver
 
-	/*public static final String url = "http://107.180.88.121:8080/HrEsayWebApi/";
-	public static String REPORT_SAVE = "/opt/apache-tomcat-8.5.47/webapps/hrdocument/Report.pdf";
-	public static final String ReportURL = "http://107.180.88.121:8080/HrEasy/";
-	public static final String attsDocSaveUrl = "/opt/apache-tomcat-8.5.47/webapps/hrdocument/attendancedoc/";
-	public static final String docSaveUrl = "/opt/apache-tomcat-8.5.47/webapps/hrdocument/updatedoc/";
-
-	public static final String companyLogoSaveUrl = "/opt/apache-tomcat-8.5.47/webapps/hrdocument/companylogo/";
-	public static final String empDocSaveUrl = "/opt/apache-tomcat-8.5.47/webapps/hrdocument/empdoc/";
-	public static final String leaveDocSaveUrl = "/opt/apache-tomcat-8.5.47/webapps/hrdocument/leavedoc/";
-	public static final String imageSaveUrl = "/opt/apache-tomcat-8.5.47/webapps/hrdocument/mixDoc/";
-
-	public static final String companyLogoShowUrl = "http://107.180.88.121:8080/hrdocument/companylogo/";
-	public static final String empDocShowUrl = "http://107.180.88.121:8080/hrdocument/empdoc/";
-	public static final String leaveDocShowUrl = "http://107.180.88.121:8080/hrdocument/leavedoc/";
-	public static final String imageShowUrl = "http://107.180.88.121:8080/hrdocument/mixDoc/";
-	public static final String templateShowUrl = "http://107.180.88.121:8080/hrdocument/templatedoc/";
-	public static String EPF_SAVE = "/opt/apache-tomcat-8.5.47/webapps/hrdocument/EPFFILE.txt";*/
+	/*
+	 * public static final String url = "http://107.180.88.121:8080/HrEsayWebApi/";
+	 * public static String REPORT_SAVE =
+	 * "/opt/apache-tomcat-8.5.47/webapps/hrdocument/Report.pdf"; public static
+	 * final String ReportURL = "http://107.180.88.121:8080/HrEasy/"; public static
+	 * final String attsDocSaveUrl =
+	 * "/opt/apache-tomcat-8.5.47/webapps/hrdocument/attendancedoc/"; public static
+	 * final String docSaveUrl =
+	 * "/opt/apache-tomcat-8.5.47/webapps/hrdocument/updatedoc/";
+	 * 
+	 * public static final String companyLogoSaveUrl =
+	 * "/opt/apache-tomcat-8.5.47/webapps/hrdocument/companylogo/"; public static
+	 * final String empDocSaveUrl =
+	 * "/opt/apache-tomcat-8.5.47/webapps/hrdocument/empdoc/"; public static final
+	 * String leaveDocSaveUrl =
+	 * "/opt/apache-tomcat-8.5.47/webapps/hrdocument/leavedoc/"; public static final
+	 * String imageSaveUrl = "/opt/apache-tomcat-8.5.47/webapps/hrdocument/mixDoc/";
+	 * 
+	 * public static final String companyLogoShowUrl =
+	 * "http://107.180.88.121:8080/hrdocument/companylogo/"; public static final
+	 * String empDocShowUrl = "http://107.180.88.121:8080/hrdocument/empdoc/";
+	 * public static final String leaveDocShowUrl =
+	 * "http://107.180.88.121:8080/hrdocument/leavedoc/"; public static final String
+	 * imageShowUrl = "http://107.180.88.121:8080/hrdocument/mixDoc/"; public static
+	 * final String templateShowUrl =
+	 * "http://107.180.88.121:8080/hrdocument/templatedoc/"; public static String
+	 * EPF_SAVE = "/opt/apache-tomcat-8.5.47/webapps/hrdocument/EPFFILE.txt";
+	 */
 
 	// Mumbai hr
 
